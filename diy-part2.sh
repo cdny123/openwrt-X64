17f238df530d23da7002c3459a6525001a728f83
cdny123 <133751542+cdny123@users.noreply.github.com>
2024-03-13 16:20:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,19 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
+# 修改默认IP地址
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
+# 修改默认内核
+#sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
+# 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
+# 修改主机名称
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+#酷猫主题高级设置
+git clone https://github.com/sirpdboy/luci-app-advancedplus/releases package/luci-app-advancedplus
+# 一键自动格式化分区、扩容、自动挂载插件
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+# ChatGPT
+git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt
+# 醋猫主题插件
+git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
