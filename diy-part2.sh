9df77b9539057cb098e506c3e7f61d6359d807df
cdny123 <133751542+cdny123@users.noreply.github.com>
2024-03-14 09:49:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,7 +21,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 #酷猫主题高级设置
 git clone https://github.com/sirpdboy/luci-app-advancedplus/releases package/luci-app-advancedplus
 # 一键自动格式化分区、扩容、自动挂载插件
-git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+#git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 # ChatGPT
 git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt
 # 醋猫主题插件
